ea0009a8dac1d7a7ae9d9a85176233ad3dc34f55
SkiletCool-desing <128517776+SkiletCool-desing@users.noreply.github.com>
2023-09-02 13:12:11 +0300

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -39,11 +39,4 @@ function createPromise(position, delay) {
       }
     }, delay);
   });
-}function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
 }
